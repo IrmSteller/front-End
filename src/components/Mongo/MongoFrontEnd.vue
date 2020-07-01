c175b790b41b1ba22d854ed683fedcbf9351df9d
IrmSteller <IrmStellerqgmx.de>
2020-07-01 16:52:03 +0200

    fetch and add

--- src/components/Mongo/MongoFrontEnd.vue
+++ src/components/Mongo/MongoFrontEnd.vue
@@ -1,15 +1,73 @@
 <template>
-  <div>
-    Hello Mongo
+  <div>    
+    <button @click="this.fetchBooks">Refresh</button>
+    <div id="container">
+        <b-card v-for="book in books" :key="book._id"
+          :title=book.name
+          tag="article"
+          style="max-width:20rem"
+          class="mb-2"
+          :footer=book.price
+        />
+    </div>
+    <p></p>
+  <form method="post" @submit.prevent="addBook">
+    <label for="name">Name</label>
+    <input type="text" name="name" v-model="name">
+    <p></p>
+    <label for="name">Price</label>
+    <input type="text" name="name" v-model="price">
+    <p></p>
+    <label for="name">Quantity</label>
+    <input type="text" name="name" v-model="quantity"> 
+    <input type="submit" value="Add">
+  </form>
   </div>
 </template>
 
 <script>
+import MongoService from '../../services/MongoService';
 export default {
-
+  comments:{
+    MongoService
+  },
+  data(){
+    return{
+      name: '',
+      price: 0,
+      quantity: 0,     
+      books: [],
+    }
+  },
+  created(){
+    this.fetchBooks()
+  },
+  methods:{
+    fetchBooks(){
+       MongoService.getAll()
+         .then(res => {this.books = res.data})
+         .catch(err => {console.log(err)})
+    },
+    addBook(){
+   
+      MongoService.addBook(this.name, this.price, this.quantity)
+      .then(res => {console.log(res)})
+      .catch(console.log("fehler"));
+    }
+  }
 }
 </script>
 
 <style>
-    
+    #container{
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    #Card{
+      margin: 10;
+      border: 1px;
+    }
+
+
 </style>
---
+++ src/services/MongoService.js
@@ -0,0 +1,24 @@
+import Axios from 'axios';
+
+const RESOURCE_NAME = (`http://localhost:3000/book/`);
+export default {
+    getAll(){
+        try {
+            return Axios.get(RESOURCE_NAME + 'getAll');
+        } catch (error) {
+            console.log(error);
+        }        
+    },
+
+    addBook(n, p, q){
+        try {
+            return Axios.post(RESOURCE_NAME + 'add', {
+                name: n,
+                price: p,
+                quantity: q
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
